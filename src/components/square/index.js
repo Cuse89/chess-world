ca10408f4fcf1ba89248a307e87d4a069a582e71
marcus <marcusguthrie111@gmail.com>
2020-03-17 14:56:17 +0100

    tiled squares

--- src/components/square/index.js
+++ src/components/square/index.js
@@ -1,19 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Droppable } from "react-beautiful-dnd";
+import cx from "classnames";
 
 import styles from "./Square.module.scss";
 
 const Square = ({ children, coords, handleOnClick }) => {
+  const isGreen = () => {
+    const isEven = n => n % 2 === 0;
+    const evenRow = isEven(Number(coords[0]));
+    const evenSquare = isEven(Number(coords[1]));
+    return evenRow ? !evenSquare : evenSquare;
+  };
+  const className = cx({
+    [styles.square]: true,
+    [styles.green]: isGreen()
+  });
   return (
     <Droppable droppableId={coords}>
       {(provided, snapshot) => {
         return (
           <div ref={provided.innerRef} {...provided.droppableProps}>
-            <div
-              className={styles.square}
-              onClick={() => handleOnClick(coords)}
-            >
+            <div className={className} onClick={() => handleOnClick(coords)}>
               {children}
             </div>
             {/*{provided.placeholder}*/}
