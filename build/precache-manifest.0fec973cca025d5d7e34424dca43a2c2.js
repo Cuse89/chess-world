11d7a1e4c669113b11c1ea85a7fa2247daeacc1c
marcus <marcusguthrie111@gmail.com>
2020-03-31 20:34:06 +0200

    include build

---
+++ build/precache-manifest.0fec973cca025d5d7e34424dca43a2c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d193681c62b9805586aa135607e81514",
+    "url": "/chess-world/index.html"
+  },
+  {
+    "revision": "c2b07a8a571bdf22aaec",
+    "url": "/chess-world/static/css/main.446bd2df.chunk.css"
+  },
+  {
+    "revision": "e8495ea6cf550eb7846b",
+    "url": "/chess-world/static/js/2.7d0fb392.chunk.js"
+  },
+  {
+    "revision": "3ee64a3963b797a3e3ba1309b348eddb",
+    "url": "/chess-world/static/js/2.7d0fb392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2b07a8a571bdf22aaec",
+    "url": "/chess-world/static/js/main.8c552026.chunk.js"
+  },
+  {
+    "revision": "15342c688324c37cbf1c",
+    "url": "/chess-world/static/js/runtime-main.430988e0.js"
+  }
+]);
