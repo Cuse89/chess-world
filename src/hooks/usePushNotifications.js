b40cd88a9b659dce5b09a157f3e0ac1eb3bd67c8
marcus <marcusguthrie111@gmail.com>
2020-04-10 16:14:50 +0200

    set up cloud functions, each device stores own notificationToken rather than per user

--- src/hooks/usePushNotifications.js
+++ src/hooks/usePushNotifications.js
@@ -4,66 +4,47 @@ import firebase from "../firebase";
 const usePushNotifications = userId => {
   const [isSubscribed, setIsSubscribed] = useState(false);
   const [message, setMessage] = useState("");
-  console.log({ isSubscribed });
 
   const listenToNotificationsSubscribe = () => {
-    firebase.getFromDatabaseListener(`users/${userId}`, user => {
-      if (!isSubscribed && user.acceptsNotifications) {
-        // get new token (if on different device)
-        subscribeToNotifications();
-        setIsSubscribed(true);
-        handleMessageListener();
-      } else if (!user.acceptsNotifications) {
-        setIsSubscribed(false);
+    firebase.getFromDatabaseListener(
+      `users/${userId}/notificationTokens`,
+      notificationTokens => {
+        firebase.messaging.getToken().then(token => {
+          const acceptsNotifications =
+            notificationTokens && notificationTokens[token];
+          if (!isSubscribed && acceptsNotifications) {
+            setIsSubscribed(true);
+            handleMessageListener();
+          } else if (!acceptsNotifications) {
+            setIsSubscribed(false);
+          }
+        });
       }
-    });
-  };
-
-  const getToken = callback => {
-    firebase.messaging
-      .getToken()
-      .then(token => {
-        if (token) {
-          console.log({ token });
-          callback(token);
-        } else {
-          // Show permission request.
-          console.log(
-            "No Instance ID token available. Request permission to generate one."
-          );
-          // Show permission UI.
-          // updateUIForPushPermissionRequired;
-        }
-      })
-      .catch(err => {
-        console.log("An error occurred while retrieving token. ", err);
-        // showToken('Error retrieving Instance ID token. ', err);
-        // setTokenSentToServer(false);
-      });
-  };
-
-  const setAcceptsNotifications = accepts => {
-    firebase.setUser(userId, "acceptsNotifications", accepts);
-  };
-
-  const subscribeToNotifications = () => {
-    console.log("subscribeToNotifications");
-    getToken(() => setAcceptsNotifications(true));
-  };
-
-  const unsubscribeToNotifications = () => {
-    console.log("unsubscribe");
-    getToken(token =>
-      firebase.messaging
-        .deleteToken(token)
-        .then(() => setAcceptsNotifications(false))
     );
   };
 
+  const setAcceptsNotifications = (token, value) => {
+    firebase.updateUser(userId, "notificationTokens", { [token]: value });
+  };
+
+  const subscribeToNotifications = () => {
+    firebase.messaging.getToken().then(token => {
+      setAcceptsNotifications(token, true);
+    });
+  };
+
+  const unsubscribeToNotifications = () => {
+    firebase.messaging
+      .getToken()
+      .then(token =>
+        firebase.messaging
+          .deleteToken(token)
+          .then(() => setAcceptsNotifications(token, null))
+      );
+  };
+
   const handleMessageListener = () => {
-    console.log("handleMessageListener");
     firebase.messaging.onMessage(payload => {
-      console.log("Message received. ", payload);
       setMessage(payload.notification.body);
     });
   };
